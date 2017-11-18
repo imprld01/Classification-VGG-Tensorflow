27954d3ee16d7119c60ccb6e66572574fb0b5f29
Bo-Wun Shih <bowns1688@hotmail.com>
2017-11-18 10:44:55 +0800

    [update] file structure

--- vgg/vgg16/detection.py
+++ vgg/vgg16/detection.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
 
-class VGG16:
+class Detector:
 
 	def __init__(self):
---
+++ vgg/vgg16/learning.py
@@ -0,0 +1,8 @@
+#import sys
+#sys.path.append("..")
+
+import tensorflow as tf
+
+class Learner:
+
+	def __init__(self):
